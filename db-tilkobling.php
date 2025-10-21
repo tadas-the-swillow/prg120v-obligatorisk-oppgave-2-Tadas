70abc200e0aa6a78a84a8b33edb606b91fd75c82
tadas-the-swillow <tadas.jog55@gmail.com>
2025-10-21 19:14:38 +0200

    test

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -1,9 +1,9 @@
 <?php
 /* kobler til databasen */
-$host = "localhost";
-$user = "root";    // endre til dokploy-bruker ved behov
-$pass = "";
-$dbnavn = "skole";
+$host = "mariadb";
+$user = "tajog5618";    // endre til dokploy-bruker ved behov
+$pass = "08d6tajog5618";
+$dbnavn = "tajog5618";
 
 $db = mysqli_connect($host, $user, $pass, $dbnavn) or die ("Ikke kontakt med database-server");
 ?>
